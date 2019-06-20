1d7ac2482c28ee6cd4e9ed1f0047d2a26844be4b
Thomas A. Schneider <thomas.alcala@gmail.com>
2019-06-20 00:29:18 -0500

    feat: mongoose

--- final/server/src/utils.js
+++ final/server/src/utils.js
@@ -1,20 +1,27 @@
-const SQL = require('sequelize');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+  email: String,
+  password: String,
+  token: String,
+  role: String,
+});
 
 module.exports.paginateResults = ({
   after: cursor,
   pageSize = 20,
   results,
-  // can pass in a function to calculate an item's cursor
   getCursor = () => null,
 }) => {
   if (pageSize < 1) return [];
 
   if (!cursor) return results.slice(0, pageSize);
   const cursorIndex = results.findIndex(item => {
-    // if an item has a `cursor` on it, use that, otherwise try to generate one
     let itemCursor = item.cursor ? item.cursor : getCursor(item);
 
-    // if there's still not a cursor, return false by default
     return itemCursor ? cursor === itemCursor : false;
   });
 
@@ -29,41 +36,13 @@ module.exports.paginateResults = ({
 };
 
 module.exports.createStore = () => {
-  const Op = SQL.Op;
-  const operatorsAliases = {
-    $in: Op.in,
-  };
-
-  const db = new SQL('database', 'username', 'password', {
-    dialect: 'sqlite',
-    storage: './store.sqlite',
-    operatorsAliases,
-    logging: false,
+  mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true});
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function() {
+    console.log('DB Connected');
   });
+  const User = mongoose.model('User', userSchema, 'User');
 
-  const users = db.define('user', {
-    id: {
-      type: SQL.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    createdAt: SQL.DATE,
-    updatedAt: SQL.DATE,
-    email: SQL.STRING,
-    token: SQL.STRING,
-  });
-
-  const trips = db.define('trip', {
-    id: {
-      type: SQL.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    createdAt: SQL.DATE,
-    updatedAt: SQL.DATE,
-    launchId: SQL.INTEGER,
-    userId: SQL.INTEGER,
-  });
-
-  return { users, trips };
+  return { User };
 };
